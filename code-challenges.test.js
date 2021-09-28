b20c85b68eae4b8ba7001a3bb699ff19f0cc0697
bluememfis <mrshaunsharp@gmail.com>
2021-09-28 07:02:24 -0700

    Trying to catch up Week 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,11 +21,54 @@
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+//PSEUDOCODE
+//This function take the number and  finds the fiboncci numbers ,
+// creates an array of the lengt number and adds them to array and return the array
 
+function fibonacci(number)
+{
+    //initialize
+    var fibonacciNumbers = [];
+    //initalize the fibbonacci increments values
+		let n1 = 1, n2 = 1, nextTerm;
+
+
+// /looop through the range
+for (let i = 1; i <= number; i++) {
+    //push the number to array
+     fibonacciNumbers .push(n1)
+//next fiboncci base start
+    nextTerm = n1 + n2;
+    // increment n1 to  n2 i.e + 1
+    n1 = n2;
+    // increment n2 to nextTerm i.e +1
+    n2 = nextTerm;
+
+}
+    //retunr the array in  json to match the other from test beacuse of serialzation problem
+    return JSON.stringify(fibonacciNumbers);
+}
+ module.exports = fibonacci;
 
 // b) Create the function that makes the test pass.
 
+const fibonacci = require('./fibonacci');//import the script
+//declaring and intializing
 
+var firstInput = 6
+var secondInput = 10
+//test fot the length 6 array fibonacci
+test('index of the first vowel', () => {
+  //expect the array of the numbers to be 
+        expect(fibonacci(firstInput)).toBe(JSON.stringify([1, 1, 2, 3, 5, 8])); 
+     
+});
+//test for array lenght 10
+test('index of the first vowel', () => {
+  //check ig the return match the expected
+        expect(fibonacci(secondInput)).toBe(JSON.stringify([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])); 
+    
+});
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
@@ -37,8 +80,84 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+//PSEUDOCODE
+//this program take the array  of items/
+// filter the array and bool values
+//push the items to new arrat items 
+// filters  the new array to remove the even numbers
+//sorts the array in ascendig
+//return the array of ord numbers
+
+function odd(myArray)
+{
+    //declare and initialize
+
+    var items = [];
+    //loop throught hte array to remove the strings and boolean
+    for (let i = 0; i <= myArray.length; i++) {
+	if(typeof myArray[i] != "string" && typeof myArray[i] !== "boolean"){
+//push to new array
+		items.push(myArray[i]);
+ 
+	}
+       
+
+    }
+    //declare and set new array to be the odd numbers from items array
+	let oddNumbersArray = items.filter(n => n%2)
+	//looop throught to sort thre array
+    for (var i = 0; i < oddNumbersArray.length - 1; i++) {
+        // declare and initialize the min
+        var min = i;
+        // loop  again //innner loop to compare the numbers and sort 
+        for (var j = i + 1; j < oddNumbersArray.length; j++) {
+        //if number at i is less than the number at j then is the min
+            if (oddNumbersArray[j] < oddNumbersArray[min]) {
+            
+            min = j;
+        }
+        }
+        // if mean is not equal to number at i the min is the number at i which is target
+    if (min != i) {
+        var target = oddNumbersArray[i];
+        oddNumbersArray[i] = oddNumbersArray[min];
+        oddNumbersArray[min] = target;
+    }
+}
+   
+    
+//retunr sorted array in json to match the test
+   
+    return JSON.stringify(oddNumbersArray);
+   
+
+
+
+}
+ module.exports = odd;
+
 
 // b) Create the function that makes the test pass.
+
+const odd = require('./odd');
+//arrays 
+var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// test/ for array one
+test('odd numbers', () => {
+        // if matcth the its a pass
+  
+        expect(odd(fullArr1)).toBe(JSON.stringify([-9, 7, 9, 199])); 
+    
+ 
+});
+//test fot array 2  
+test('odd numbers', () => {
+//   match is a pass/
+        expect(odd(fullArr2)).toBe(JSON.stringify([-823, 7, 23])); 
+    
+ 
+}); 
 
 
 
@@ -55,6 +174,50 @@ var numbersToAdd2 = [0, 7, -8, 12]
 var numbersToAdd3 = []
 // Expected output: []
 
+//PSEUDOCODE
+//fuction takes array and sums  the items in "factorial";
+//using the reduce we can loop and sun the previous number that is added to the new value frome the array
+function odd(myArray)
+{
+    //new aray to hold the sum
+    let sumArray = [];
+    //loop and sum the previous withe current index value
+    myArray.reduce( (prev, curr,i) =>  sumArray[i] = prev + curr , 0 )
+    
+    //return thr array in json to match the test beacuse os serialization
+    return JSON.stringify(sumArray);
+
+}
+ module.exports = odd;
 
 
 // b) Create the function that makes the test pass.
+
+const sum = require('./sum');
+//declare and intilization
+var numbersToAdd1 = [2, 4, 45, 9];
+var numbersToAdd2 = [0, 7, -8, 12];
+var numbersToAdd3 = []
+// test // for array 1
+//these test can be looped []
+test('sum accumulated', () => {
+  
+        expect(sum(numbersToAdd1)).toBe(JSON.stringify([2, 6, 51, 60])); 
+    
+ 
+});
+// test for array 2
+
+test('sum accumulated', () => {
+  
+        expect(sum(numbersToAdd2)).toBe(JSON.stringify([0, 7, -1, 11])); 
+    
+ 
+});
+//test for array 3
+test('sum accumulated', () => {
+  
+        expect(sum(numbersToAdd3)).toBe(JSON.stringify([])); 
+    
+ 
+}); 
--- interview-questions.md
+++ interview-questions.md
@@ -9,49 +9,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: It is whatever it is belongs to. It also can be global or undefined. 
 
-  Researched answer:
-
+  Researched answer:  Alone it is global object. In a function, this refers to the global object and with the strict mode(something I just learned about RN!!!) it is undefined as well. 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: It using Javascript, and is a open source. React is used to make simple simple interfaces...Facebook made it.
 
-  Researched answer:
+  Researched answer:  React is a open-source front-end JavaScript library for building user interfaces or UI components. maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: Render
 
-  Researched answer:
+  Researched answer: Each component needs to have an render method. The render method contains all the information that will be displayed.
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: It is something that helps us read HTML in. 
 
-  Researched answer:
+  Researched answer: HTML has access to the Real DOM at all times, JSX works off a virtual clone of DOM. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a package manager and it used to share code globally.
 
-  Researched answer:
+  Researched answer:  Yarn is a package manager for your code. It allows you to use and share (e.g. JavaScript) code with other developers from around the world.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: Function with no name...?
 
-  Researched answer:
+  Researched answer:  Anonymous function does not have any name associated with it. And in anonymous functions in JavaScript, only use the function keyword without the function name.
 
 
 ## Looking Ahead: Terms for Next Week
